98489183d9d048bd33e4dda471e7ddf9a625bd39
Andy Chen <andycjx8@gmail.com>
2022-01-29 11:19:50 +1100

    Implement getting movie by id

--- src/Components/movieForm.jsx
+++ src/Components/movieForm.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { getMovie } from "./../Services/movieService";
 
 class MovieForm extends Component {
-  state = {};
+  state = { movieDetails: {} };
+
+  componentDidMount() {
+    const movieId = this.props.match.params.id;
+    getMovie(movieId)
+      .catch((error) => console.log("There is an error" + error))
+      .then((result) => {
+        this.setState({ movieDetails: result.data });
+      });
+  }
+
   render() {
+    console.log(this.state.movieDetails);
     return <h1>{this.props.match.params.id}</h1>;
   }
 }
