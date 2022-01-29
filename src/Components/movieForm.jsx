f1f9e1c20eadf614f754bf839c947b6d1853072b
Andy Chen <andycjx8@gmail.com>
2022-01-29 11:36:47 +1100

    Map details of the movie into the state

--- src/Components/movieForm.jsx
+++ src/Components/movieForm.jsx
@@ -1,20 +1,46 @@
 import React, { Component } from "react";
-import { getMovie } from "./../Services/movieService";
+import { getMovie, mapToViewMovie } from "./../Services/movieService";
 
 class MovieForm extends Component {
-  state = { movieDetails: {} };
+  state = {
+    movieDetails: {
+      Title: "",
+      Year: "",
+      Rated: "",
+      Released: "",
+      Runtime: "",
+      Genre: "",
+      Director: "",
+      Writer: "",
+      Actors: "",
+      Plot: "",
+      Language: "",
+      Country: "",
+      Awards: "",
+      Poster: "",
+      MetaScore: "",
+      imdbRating: "",
+      imdbVotes: "",
+      imdbID: "",
+      Type: "",
+      DVD: "",
+      BoxOffice: "",
+      Production: "",
+      Website: "",
+      Response: "",
+    },
+  };
 
   componentDidMount() {
     const movieId = this.props.match.params.id;
     getMovie(movieId)
       .catch((error) => console.log("There is an error" + error))
       .then((result) => {
-        this.setState({ movieDetails: result.data });
+        this.setState({ movieDetails: mapToViewMovie(result.data) });
       });
   }
 
   render() {
-    console.log(this.state.movieDetails);
     return <h1>{this.props.match.params.id}</h1>;
   }
 }
