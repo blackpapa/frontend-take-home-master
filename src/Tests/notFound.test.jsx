004efa31d33ded2b1431174e99b2a1af0b0aaef4
Andy Chen <andycjx8@gmail.com>
2022-01-30 11:02:26 +1100

    Update the test

---
+++ src/Tests/notFound.test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import NotFound from "../Components/notFound";
+
+afterEach(cleanup);
+
+describe("Not Found page", () => {
+  it("should be working", () => {
+    const { container, getByText } = render(<NotFound />);
+
+    expect(getByText("Not Found")).toBeInTheDocument();
+  });
+});
